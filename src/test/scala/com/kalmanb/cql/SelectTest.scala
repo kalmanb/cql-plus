5f2bcfdff3111aea783dcffa53c3f076759f47a5
kalmanb <kalman@movio.co>
2013-09-08 23:38:45 +1200

    Clean up tests

--- src/test/scala/com/kalmanb/cql/SelectTest.scala
+++ src/test/scala/com/kalmanb/cql/SelectTest.scala
@@ -5,9 +5,22 @@ import com.kalmanb.test.TestSpec
 class SelectTest extends TestSpec {
 
   describe("describe") {
+    it("simple select query") {
+      /**
+       * Memeber [
+       *   id : Long
+       *   firstName : String
+       *   dob : Long
+       * ]
+       */
+      val ecql = "+select * from member where id = 123"
 
-  it("should") {
-    /* cursor */
-  }
+      val cql = "select key, f1, f2 from genericTable where key = 'client1|member|123'"
+
+      //val result = Row()
+      // row.getLong("id") = 123
+      // row.getString("firstName") = "first"
+      // row.getLong("dob") = 123456
+    }
   }
 }
